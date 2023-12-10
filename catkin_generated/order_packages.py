946a15ad5c75d004663c0470f8c800c1a857c2d6
Jaskins32 <144564985+Jaskins32@users.noreply.github.com>
2023-12-10 14:18:11 -0800

    Add files via upload

---
+++ catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/jasona/catkin_ws/src'
+whitelisted_packages = 'launch_line_follower_bot'.split(';') if 'launch_line_follower_bot' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/jasona/catkin_ws/devel;/opt/ros/melodic'.split(';') if '/home/jasona/catkin_ws/devel;/opt/ros/melodic' != '' else []
